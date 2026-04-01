0110b28848fd5c7c9fd544a0cef8019bafd248bc
stone-py <35140673+stone-py@users.noreply.github.com>
2026-04-01 14:41:51 +0800

    Update setup.py to include package discovery

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-
 # This is a shim to hopefully allow Github to detect the package, build is done with poetry
-
 import setuptools
-
 if __name__ == "__main__":
-    setuptools.setup(name="baidupcs-py")
+    setuptools.setup(
+        name="baidupcs-py",
+        packages=setuptools.find_packages(include=["baidupcs_py", "baidupcs_py.*"]),
+    )
