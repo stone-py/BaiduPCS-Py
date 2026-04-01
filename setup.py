f7b9e8019631f765574ad52434b0f816a8f689d6
stone-py <35140673+stone-py@users.noreply.github.com>
2026-04-01 16:32:22 +0800

    Integrate Cython for simple_cipher module

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
-# This is a shim to hopefully allow Github to detect the package, build is done with poetry
 import setuptools
+from Cython.Build import cythonize
+import os
+
+os.environ["CFLAGS"] = "-O3"
+
 if __name__ == "__main__":
+    ext_modules = cythonize(
+        ["baidupcs_py/common/simple_cipher.pyx"],
+        language_level=3,
+        compiler_directives={"linetrace": True},
+    )
+    ext_modules[0].name = "baidupcs_py.common.simple_cipher"
+
     setuptools.setup(
         name="baidupcs-py",
         packages=setuptools.find_packages(include=["baidupcs_py", "baidupcs_py.*"]),
+        ext_modules=ext_modules,
     )
